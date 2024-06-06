9c1b12a4f50c0703442ad78e8c401405e7fecad5
Erick Reyes <reyespoloerick1@gmail.com>
2024-06-06 15:31:50 -0500

    Modifique tipo de fuente

--- ProyectoMoanso/FormPrincipal.Designer.cs
+++ ProyectoMoanso/FormPrincipal.Designer.cs
@@ -62,7 +62,7 @@
             this.panelTitulo.Controls.Add(this.btnRestaurar);
             this.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTitulo.Location = new System.Drawing.Point(0, 0);
-            this.panelTitulo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelTitulo.Margin = new System.Windows.Forms.Padding(4);
             this.panelTitulo.Name = "panelTitulo";
             this.panelTitulo.Size = new System.Drawing.Size(1469, 80);
             this.panelTitulo.TabIndex = 4;
@@ -74,7 +74,7 @@
             this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
             this.btnMinimizar.InitialImage = null;
             this.btnMinimizar.Location = new System.Drawing.Point(1323, 15);
-            this.btnMinimizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMinimizar.Margin = new System.Windows.Forms.Padding(4);
             this.btnMinimizar.Name = "btnMinimizar";
             this.btnMinimizar.Size = new System.Drawing.Size(44, 43);
             this.btnMinimizar.TabIndex = 4;
@@ -85,7 +85,7 @@
             // 
             this.btnMaximizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMaximizar.Image")));
             this.btnMaximizar.Location = new System.Drawing.Point(1375, 15);
-            this.btnMaximizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMaximizar.Margin = new System.Windows.Forms.Padding(4);
             this.btnMaximizar.Name = "btnMaximizar";
             this.btnMaximizar.Size = new System.Drawing.Size(39, 43);
             this.btnMaximizar.TabIndex = 2;
@@ -97,7 +97,7 @@
             this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
             this.btnCerrar.Location = new System.Drawing.Point(1427, 15);
-            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(39, 43);
             this.btnCerrar.TabIndex = 1;
@@ -107,12 +107,12 @@
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Font = new System.Drawing.Font("Roboto", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.ForeColor = System.Drawing.SystemColors.Window;
             this.lblTitulo.Location = new System.Drawing.Point(567, 36);
             this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(323, 24);
+            this.lblTitulo.Size = new System.Drawing.Size(290, 22);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "FERRETERIA SAN JUAN LINKOL";
             // 
@@ -121,7 +121,7 @@
             this.btnRestaurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnRestaurar.Image = ((System.Drawing.Image)(resources.GetObject("btnRestaurar.Image")));
             this.btnRestaurar.Location = new System.Drawing.Point(1375, 15);
-            this.btnRestaurar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRestaurar.Margin = new System.Windows.Forms.Padding(4);
             this.btnRestaurar.Name = "btnRestaurar";
             this.btnRestaurar.Size = new System.Drawing.Size(44, 43);
             this.btnRestaurar.TabIndex = 3;
@@ -140,7 +140,7 @@
             this.panelOpciones.Controls.Add(this.btnOrdenCompra);
             this.panelOpciones.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelOpciones.Location = new System.Drawing.Point(0, 80);
-            this.panelOpciones.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelOpciones.Margin = new System.Windows.Forms.Padding(4);
             this.panelOpciones.Name = "panelOpciones";
             this.panelOpciones.Size = new System.Drawing.Size(213, 636);
             this.panelOpciones.TabIndex = 5;
@@ -149,11 +149,11 @@
             // 
             this.btnProovedores.BackColor = System.Drawing.Color.WhiteSmoke;
             this.btnProovedores.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnProovedores.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProovedores.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnProovedores.Image = ((System.Drawing.Image)(resources.GetObject("btnProovedores.Image")));
             this.btnProovedores.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnProovedores.Location = new System.Drawing.Point(5, 454);
-            this.btnProovedores.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnProovedores.Margin = new System.Windows.Forms.Padding(4);
             this.btnProovedores.Name = "btnProovedores";
             this.btnProovedores.Size = new System.Drawing.Size(208, 65);
             this.btnProovedores.TabIndex = 7;
@@ -165,11 +165,11 @@
             // 
             this.btnDevolucion.BackColor = System.Drawing.Color.WhiteSmoke;
             this.btnDevolucion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnDevolucion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDevolucion.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDevolucion.Image = ((System.Drawing.Image)(resources.GetObject("btnDevolucion.Image")));
             this.btnDevolucion.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnDevolucion.Location = new System.Drawing.Point(5, 382);
-            this.btnDevolucion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDevolucion.Margin = new System.Windows.Forms.Padding(4);
             this.btnDevolucion.Name = "btnDevolucion";
             this.btnDevolucion.Size = new System.Drawing.Size(208, 65);
             this.btnDevolucion.TabIndex = 6;
@@ -181,11 +181,11 @@
             // 
             this.btnPagoFacturas.BackColor = System.Drawing.Color.WhiteSmoke;
             this.btnPagoFacturas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnPagoFacturas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPagoFacturas.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPagoFacturas.Image = ((System.Drawing.Image)(resources.GetObject("btnPagoFacturas.Image")));
             this.btnPagoFacturas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnPagoFacturas.Location = new System.Drawing.Point(4, 164);
-            this.btnPagoFacturas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPagoFacturas.Margin = new System.Windows.Forms.Padding(4);
             this.btnPagoFacturas.Name = "btnPagoFacturas";
             this.btnPagoFacturas.Size = new System.Drawing.Size(208, 65);
             this.btnPagoFacturas.TabIndex = 5;
@@ -197,11 +197,11 @@
             // 
             this.btnProductos.BackColor = System.Drawing.Color.WhiteSmoke;
             this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProductos.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnProductos.Image = ((System.Drawing.Image)(resources.GetObject("btnProductos.Image")));
             this.btnProductos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnProductos.Location = new System.Drawing.Point(5, 309);
-            this.btnProductos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnProductos.Margin = new System.Windows.Forms.Padding(4);
             this.btnProductos.Name = "btnProductos";
             this.btnProductos.Size = new System.Drawing.Size(208, 65);
             this.btnProductos.TabIndex = 4;
@@ -214,11 +214,11 @@
             // 
             this.btnAlmacen.BackColor = System.Drawing.Color.WhiteSmoke;
             this.btnAlmacen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnAlmacen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlmacen.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAlmacen.Image = ((System.Drawing.Image)(resources.GetObject("btnAlmacen.Image")));
             this.btnAlmacen.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnAlmacen.Location = new System.Drawing.Point(4, 236);
-            this.btnAlmacen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAlmacen.Margin = new System.Windows.Forms.Padding(4);
             this.btnAlmacen.Name = "btnAlmacen";
             this.btnAlmacen.Size = new System.Drawing.Size(208, 65);
             this.btnAlmacen.TabIndex = 3;
@@ -230,11 +230,11 @@
             // 
             this.btnNecesidad.BackColor = System.Drawing.Color.WhiteSmoke;
             this.btnNecesidad.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnNecesidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNecesidad.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNecesidad.Image = ((System.Drawing.Image)(resources.GetObject("btnNecesidad.Image")));
             this.btnNecesidad.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnNecesidad.Location = new System.Drawing.Point(4, 91);
-            this.btnNecesidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNecesidad.Margin = new System.Windows.Forms.Padding(4);
             this.btnNecesidad.Name = "btnNecesidad";
             this.btnNecesidad.Size = new System.Drawing.Size(208, 65);
             this.btnNecesidad.TabIndex = 2;
@@ -246,11 +246,11 @@
             // 
             this.btnOrdenCompra.BackColor = System.Drawing.Color.WhiteSmoke;
             this.btnOrdenCompra.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnOrdenCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenCompra.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOrdenCompra.Image = ((System.Drawing.Image)(resources.GetObject("btnOrdenCompra.Image")));
             this.btnOrdenCompra.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnOrdenCompra.Location = new System.Drawing.Point(4, 18);
-            this.btnOrdenCompra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOrdenCompra.Margin = new System.Windows.Forms.Padding(4);
             this.btnOrdenCompra.Name = "btnOrdenCompra";
             this.btnOrdenCompra.Size = new System.Drawing.Size(208, 65);
             this.btnOrdenCompra.TabIndex = 1;
@@ -264,7 +264,7 @@
             this.contenedor.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.contenedor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.contenedor.Location = new System.Drawing.Point(213, 80);
-            this.contenedor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.contenedor.Margin = new System.Windows.Forms.Padding(4);
             this.contenedor.Name = "contenedor";
             this.contenedor.Size = new System.Drawing.Size(1256, 636);
             this.contenedor.TabIndex = 6;
--- ProyectoMoanso/Productos.Designer.cs
+++ ProyectoMoanso/Productos.Designer.cs
@@ -52,37 +52,37 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Roboto", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(53, 110);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(161, 20);
+            this.label1.Size = new System.Drawing.Size(167, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Código de Producto:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Roboto", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(53, 163);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.Size = new System.Drawing.Size(75, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Roboto", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(53, 207);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.Size = new System.Drawing.Size(57, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Stock:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Roboto", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(53, 249);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 20);
@@ -92,10 +92,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Roboto", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(53, 289);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.Size = new System.Drawing.Size(87, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Categoría:";
             // 
@@ -148,16 +148,16 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Roboto", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(139, 30);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(413, 32);
+            this.label6.Size = new System.Drawing.Size(379, 34);
             this.label6.TabIndex = 11;
             this.label6.Text = "REGISTRO DE PRODUCTOS";
             // 
             // btn_Eliminar_Produc
             // 
-            this.btn_Eliminar_Produc.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Eliminar_Produc.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Eliminar_Produc.Image = global::ProyectoMoanso.Properties.Resources.bote_de_basura;
             this.btn_Eliminar_Produc.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_Eliminar_Produc.Location = new System.Drawing.Point(530, 237);
@@ -169,7 +169,7 @@
             // 
             // btn_Actualizar_Produc
             // 
-            this.btn_Actualizar_Produc.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Actualizar_Produc.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Actualizar_Produc.Image = global::ProyectoMoanso.Properties.Resources.reset;
             this.btn_Actualizar_Produc.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_Actualizar_Produc.Location = new System.Drawing.Point(530, 163);
@@ -181,7 +181,7 @@
             // 
             // btn_Guardar_Produc
             // 
-            this.btn_Guardar_Produc.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Guardar_Produc.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Guardar_Produc.Image = global::ProyectoMoanso.Properties.Resources.Disco;
             this.btn_Guardar_Produc.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_Guardar_Produc.Location = new System.Drawing.Point(530, 85);
