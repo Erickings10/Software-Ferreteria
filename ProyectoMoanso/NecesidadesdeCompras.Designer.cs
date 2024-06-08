b0ebc7a876ab1a093d5c24f1213250521082ceae
Edwarxy <edwarsolorzano05@gmail.com>
2024-06-07 20:38:23 -0500

    Añadir el forms Necesidades de compras

---
+++ ProyectoMoanso/NecesidadesdeCompras.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace ProyectoMoanso
+{
+    partial class NecesidadesdeCompras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_editarRequerimiento = new System.Windows.Forms.Button();
+            this.btn_eliminarRequerimiento = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_cantidad = new System.Windows.Forms.TextBox();
+            this.txt_marca = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Nombredelproyecto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Codigodeproducto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_elaborarRequerimiento = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_editarRequerimiento
+            // 
+            this.btn_editarRequerimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_editarRequerimiento.Location = new System.Drawing.Point(641, 215);
+            this.btn_editarRequerimiento.Name = "btn_editarRequerimiento";
+            this.btn_editarRequerimiento.Size = new System.Drawing.Size(135, 52);
+            this.btn_editarRequerimiento.TabIndex = 34;
+            this.btn_editarRequerimiento.Text = "Editar Requerimiento";
+            this.btn_editarRequerimiento.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_editarRequerimiento.UseVisualStyleBackColor = true;
+            // 
+            // btn_eliminarRequerimiento
+            // 
+            this.btn_eliminarRequerimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_eliminarRequerimiento.Location = new System.Drawing.Point(641, 297);
+            this.btn_eliminarRequerimiento.Name = "btn_eliminarRequerimiento";
+            this.btn_eliminarRequerimiento.Size = new System.Drawing.Size(135, 49);
+            this.btn_eliminarRequerimiento.TabIndex = 33;
+            this.btn_eliminarRequerimiento.Text = "Eliminar Requerimiento";
+            this.btn_eliminarRequerimiento.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_eliminarRequerimiento.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(51, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 15);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Nombre del Producto";
+            // 
+            // txt_cantidad
+            // 
+            this.txt_cantidad.Location = new System.Drawing.Point(433, 105);
+            this.txt_cantidad.Name = "txt_cantidad";
+            this.txt_cantidad.Size = new System.Drawing.Size(100, 20);
+            this.txt_cantidad.TabIndex = 31;
+            // 
+            // txt_marca
+            // 
+            this.txt_marca.Location = new System.Drawing.Point(433, 146);
+            this.txt_marca.Name = "txt_marca";
+            this.txt_marca.Size = new System.Drawing.Size(100, 20);
+            this.txt_marca.TabIndex = 30;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(356, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 15);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Marca";
+            // 
+            // txt_Nombredelproyecto
+            // 
+            this.txt_Nombredelproyecto.Location = new System.Drawing.Point(213, 144);
+            this.txt_Nombredelproyecto.Name = "txt_Nombredelproyecto";
+            this.txt_Nombredelproyecto.Size = new System.Drawing.Size(100, 20);
+            this.txt_Nombredelproyecto.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(356, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Cantidad";
+            // 
+            // txt_Codigodeproducto
+            // 
+            this.txt_Codigodeproducto.Location = new System.Drawing.Point(213, 105);
+            this.txt_Codigodeproducto.Name = "txt_Codigodeproducto";
+            this.txt_Codigodeproducto.Size = new System.Drawing.Size(100, 20);
+            this.txt_Codigodeproducto.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(51, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 15);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Codigo del producto";
+            // 
+            // btn_elaborarRequerimiento
+            // 
+            this.btn_elaborarRequerimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_elaborarRequerimiento.Location = new System.Drawing.Point(641, 135);
+            this.btn_elaborarRequerimiento.Name = "btn_elaborarRequerimiento";
+            this.btn_elaborarRequerimiento.Size = new System.Drawing.Size(135, 53);
+            this.btn_elaborarRequerimiento.TabIndex = 24;
+            this.btn_elaborarRequerimiento.Text = "Elaborar Requerimiento";
+            this.btn_elaborarRequerimiento.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_elaborarRequerimiento.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 243);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(564, 103);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Codigo de Producto";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre del Producto";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cantidad";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Marca";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(207, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 33);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Necesidades de Compras";
+            // 
+            // NecesidadesdeCompras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_editarRequerimiento);
+            this.Controls.Add(this.btn_eliminarRequerimiento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_cantidad);
+            this.Controls.Add(this.txt_marca);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Nombredelproyecto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Codigodeproducto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_elaborarRequerimiento);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NecesidadesdeCompras";
+            this.Text = "NecesidadesdeCompras";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_editarRequerimiento;
+        private System.Windows.Forms.Button btn_eliminarRequerimiento;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_cantidad;
+        private System.Windows.Forms.TextBox txt_marca;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Nombredelproyecto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Codigodeproducto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_elaborarRequerimiento;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
+    }
+}
